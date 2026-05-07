8e93accb868cf16858eadb68865a59a2ee69f458
Bientechsavvy <olawalesossagbetibienvenu@gmail.com>
2026-05-07 14:32:51 +0100

    Update CORS origin to new IP address

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const { authLimiter, apiLimiter } = require('./middleware/rateLimiter');
 const app = express();
 
 app.use(cors({
-  origin: ['http://35.180.66.115:5173', 'http://localhost:5173'],
+  origin: ['http://35.180.73.174:5173', 'http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-API-Version'],
